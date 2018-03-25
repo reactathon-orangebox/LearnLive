4a22112c492b413ed4106e0d1489ca3788bee8ed
Andrew Kang <andrewsjkang@gmail.com>
2018-03-24 17:50:05 -0700

    Implement Classroom component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {loading: false, msg: null};
-  }
-
-  handleClick = (e) => {
-    e.preventDefault();
-
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
-  }
-
-  render() {
-    const {loading, msg} = this.state;
-
-    return <p>
-      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-      <span>{msg}</span>
-    </p>
-  }
-}
+import Classroom from './components/classroom/Classroom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentView: 'room'
+    }
+  }
+
+  switchView() {
+    var currentView = this.state.currentView;
+
+    if (currentView === 'room') {
+      return <Classroom />
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LambdaDemo/>
+      <div>
+        { this.switchView() }
       </div>
-    );
+    )
   }
 }
 
