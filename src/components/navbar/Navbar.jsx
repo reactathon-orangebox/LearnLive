f976e847a1632a4676ee5b0e21a842b685b59b66
Derrick Zhang <dcheung41@gmail.com>
2018-03-24 23:26:55 -0700

    updated stylesheet and navbar with search

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 //icon bar x 3, used to make hamburger mobile nav menu on top right corner.
-
 const iconBar = Array(3).fill('icon-bar')
-const navBar = ['Login']
+const navBar = ['Login', 'Sign Up']
 
 const Navbar = (props) => (
 
@@ -11,14 +10,21 @@ const Navbar = (props) => (
     <nav className="navbar navbar-default navbar-fixed-top shadow-border">
     	<div className="container-fluid">
 
+          <form className="navbar-form navbar-left pull-right">
+            <div className="form-group">
+                <input type="text" className="form-control" placeholder="Search"/>
+            </div>
+            <button type="submit" className="btn btn-warning">Search</button>
+          </form>
 
-    		<div className="navbar-header">
-    			<button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
-    				<span className="sr-only">Toggle navigation</span> 
-    				{iconBar.map(bar => <span className={bar}></span>)}
-    			</button> 
-    			<a className="navbar-brand" href="#home"><img className="navbar-brand-logo padding"/></a>
-    		</div> 
+          <div className="navbar-header pull-right">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" className="navbar-toggle">
+                    <span className="sr-only">Toggle navigation</span> 
+                    {iconBar.map(bar => <span className={bar}></span>)}
+                </button> 
+                <a className="navbar-brand" href="#home"><img className="navbar-brand-logo padding"/></a>
+        </div> 
+
 
 
     		<div id="navbarCollapse" className="collapse navbar-collapse">
