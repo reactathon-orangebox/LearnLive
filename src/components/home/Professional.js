c094ec8ffef38f40bcf30e67adb36f85309e9735
Derrick Zhang <dcheung41@gmail.com>
2018-03-25 04:39:47 -0700

    added Professionals and changed hero page, added css

--- src/components/home/Professional.js
+++ src/components/home/Professional.js
@@ -4,7 +4,6 @@ import { Icon } from 'semantic-ui-react';
 export default class Professional extends Component {
 	render() {
 		return (
-
 				<div className= "thumbnail col-md-3 col-sm-6 col-xs-12">
 					
 						<img src="https://images.pexels.com/photos/378148/pexels-photo-378148.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
