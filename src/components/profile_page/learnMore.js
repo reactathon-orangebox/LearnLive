04b1861d5e18e81765b51893252b7f4ff3775ea7
Larry <lchang2190@gmail.com>
2018-03-25 02:26:22 -0700

    static with no handelers

---
+++ src/components/profile_page/learnMore.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Container, Header } from 'semantic-ui-react'
+
+const ContainerExampleFluid = () => (
+  <div>
+    <Container fluid>
+      <Header as='h2'>Derrick Zhang</Header>
+      <p>
+		    Derrick Zhang began practicing yoga in 2002 after migrating to San Diego from the East Coast. She completed her first teacher training nearly 10 years ago (Yoga Works, 2005). Derrick has regularly expanded her knowledge since then by attending Vinyasa Flow, Anusara, and various yoga trainings. She is also a student of Pacific College of Oriental Medicine and a massage therapist.
+				Derrick has traveled solo though India and Thailand, diving into the roots of yoga, massage and meditation traditions, and now leads annual retreats to these regions.
+				Her classes range from peaceful and relaxing (shanti shanti) to energetic, sweat-inducing vinyasa flows. She infuses her passion for yoga in each posture, giving gentle and encouraging cues with particular emphasis on the breath, playfulness, and enjoyment of the present moment.
+      </p>
+      <Header as='h2'>Classes for Spring 2018</Header>
+      <p>
+      	Start: April 1st, 2018
+      </p>
+      <p>
+      	End: July 23rd, 2018
+      </p>
+      <p>
+		    Days: Monday, Wednesday, Friday
+		  </p>
+		  <p>
+		    Time: 7pm - 8pm EST
+      </p>
+    </Container>
+  </div>
+)
+
+export default ContainerExampleFluid
