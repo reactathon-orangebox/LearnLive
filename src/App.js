798b04c58816bb0037ea7aa3cac0fd32757cd177
Stephen Choe <stphnchoe@gmail.com>
2018-03-25 13:44:02 -0700

    Changes App from jx to jsx and updates spacing on subjectsview to accomodate Navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import Profile from './components/profile_page/profile.js';
+import Navbar from './components/navbar/Navbar2.js'
 import Hero from './components/home/Hero.js';
+import SubjectsView from './components/subjects/SubjectsView.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentView: 'profile',
+      currentView: 'subjects',
       currentUser: 'larrychangg',
-
     }
+    this.onTeacherClick = this.onTeacherClick.bind(this);
   }
 
   userLogin(user) {
     this.setState({ currentUser: user });
+  }
+
+  onTeacherClick(view) {
+    this.setState({ currentView: view });
   }
 
   switchView() {
@@ -22,7 +28,9 @@ class App extends Component {
     if(currentView === 'profile') {
       return <Profile user={this.state.currentUser} />
     } else if(currentView === 'home'){
-      return <Hero />
+      return <Hero /> 
+    } else if ( currentView === 'subjects') {
+      return <SubjectsView onTeacherClick={this.onTeacherClick}/>
     }
   }
 
--- src/components/subjects/SubjectsView.jsx
+++ src/components/subjects/SubjectsView.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Navbar from '../navbar/Navbar';
 import Subject from './Subject';
 import { data } from './lib/exampleData';
+import Navbar from '../navbar/Navbar2';
 
 const SubjectsView = ({ onTeacherClick }) => (
   <div>
     <Navbar />
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
     <div className="subject-view-header">
       <h1 className="page-header">Courses</h1>
     </div>
