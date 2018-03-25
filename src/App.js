6e5f29393416584a563a458290f46394c44f4dd5
Derrick Zhang <dcheung41@gmail.com>
2018-03-25 14:54:20 -0700

    ?

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentView: 'home',
+      currentView: 'profile',
       currentUser: 'larrychangg',
     }
     this.onTeacherClick = this.onTeacherClick.bind(this);
