5dfc903d9e90f871a00f571d955901cbb7477e3f
Larry <lchang2190@gmail.com>
2018-03-24 23:21:55 -0700

    move to netlify

---
+++ src/components/profile_page/uploads.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Image, List, Modal } from 'semantic-ui-react';
+
+const Uploads = (props) => (
+	<div>
+		Hello
+	</div>
+);
+
+export default Uploads;
---
+++ src/components/profile_page/user.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Image, List } from 'semantic-ui-react';
+
+const User = (props) => (
+	<div>
+		<div className='profileHeader'>
+			<List selection verticalAlign='middle'>
+		    <List.Item>
+		      <Image avatar src='https://www.yogajournal.com/.image/t_share/MTQ2MjA3NTgyMzI2MTA1MTA1/yjlive-bow-pose.jpg' />
+		      <List.Content>
+		        <List.Header>{props.loggedInUser}</List.Header>
+		        <p className='followers'>Followers: {props.followers} &nbsp; Following: {props.following}</p>
+		      </List.Content>
+		    </List.Item>
+		   </List>
+		</div>
+	</div>
+);
+
+export default User;
